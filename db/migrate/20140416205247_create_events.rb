e245eca1e51d028507bf76c44eae499749b1f36c
Ronan McHugh <mchugh.r@gmail.com>
2014-04-16 22:53:58 +0200

    #4 ran model generator

---
+++ db/migrate/20140416205247_create_events.rb
@@ -0,0 +1,15 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.text :description
+      t.text :location
+      t.datetime :start_time
+      t.datetime :end_time
+      t.string :category
+      t.string :creator
+
+      t.timestamps
+    end
+  end
+end
