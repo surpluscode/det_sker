97e59e1c301b6e74857b5522a2daa8cde5ea8439
Ronan McHugh <mchugh.r@gmail.com>
2014-04-19 13:43:07 +0200

    expand event with validations and test for these.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ActiveRecord::Base
-  attr_accessor :title, :description
+  attr_accessor :title, :description, :location, :start_time, :end_time, :category, :creator
+  validates :title, :description, :location, :start_time, :end_time, :category, :creator, presence: true
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,8 +1,40 @@
 require 'spec_helper'
 
 describe Event do
+  before(:each) do
+    @party_details = {title: 'Massive party', creator: 'FestAbe99', description: 'The Best Party Ever!',
+                   location: 'yourbackyard', start_time: DateTime.new, end_time: DateTime.new,
+                   category: 'MadParty'}
+  end
   it 'should create an event' do
-    event = Event.new(title: 'Massive party', creator: 'FestAbe99', description: 'The Best Party Ever!')
+    event = Event.create(@party_details)
+    event.valid?.should be_true
     event.title.should match('Massive party')
   end
+  it 'should not save an event without a title' do
+    @party_details.delete(:title)
+    Event.create(@party_details).valid?.should_not be_true
+  end
+  it 'should not save an event without a location' do
+    @party_details.delete(:location)
+    Event.create(@party_details).valid?.should_not be_true
+  end
+  it 'should not save an event without a description' do
+    @party_details.delete(:description)
+    Event.create(@party_details).valid?.should_not be_true
+  end
+  it 'should not save an event without a category' do
+    @party_details.delete(:category)
+    Event.create(@party_details).valid?.should_not be_true
+  end
+  it 'should not save an event without a start_time' do
+    @party_details.delete(:start_time)
+    Event.create(@party_details).valid?.should_not be_true
+  end
+
+  it 'should not save an event without an end_time' do
+    @party_details.delete(:end_time)
+    Event.create(@party_details).valid?.should_not be_true
+  end
+
 end
