a13698c1c4242e953d9d0d8e0e5eba212ab8ae5e
Ronan McHugh <mchugh.r@gmail.com>
2014-04-20 23:00:01 +0200

    attr_accessor shouldn't be used in Rails 4.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,3 @@
 class Event < ActiveRecord::Base
-  attr_accessor :title, :description, :location, :start_time, :end_time, :category, :creator
   validates :title, :description, :location, :start_time, :end_time, :category, :creator, presence: true
 end
