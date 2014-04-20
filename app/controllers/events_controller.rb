a26a48cc85b30b81517b81e0e20b0cafd16de39b
Ronan McHugh <mchugh.r@gmail.com>
2014-04-20 22:34:56 +0200

    #5 add param validation to new method

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,8 +7,25 @@ class EventsController < ApplicationController
   end
 
   def create
+    @event = Event.new(user_params)
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to @event, notice: 'Event was created successfully.'}
+        format.json { render action: 'show', status: :created, location: @event }
+      else
+        Rails.logger.info @event.errors.full_messages
+        format.html { render action: 'new' }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def edit
+  end
+
+  private
+
+  def user_params
+    params.require(:event).permit(:title, :description, :creator, :category, :location, :start_time, :end_time)
   end
 end
