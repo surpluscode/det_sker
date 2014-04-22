5b2285d6d0b92f70473757658fa0108b2f49b662
Ronan McHugh <mchugh.r@gmail.com>
2014-04-22 21:11:50 +0200

    Index view sorts events by date.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,21 @@
 class Event < ActiveRecord::Base
   validates :title, :description, :location, :start_time, :end_time, :category, :creator, presence: true
+
+  # This method returns the index view
+  # used by EventController to list all events
+  # ordered by start-time
+  # returns structure as follows:
+  # {date1: [EventA, EventB], date2: [EventC, EventD]}
+  def self.index
+    events = self.order(:start_time)
+    ordered_by_date = {}
+    events.each do |event|
+      start_date = event.start_time.to_date
+      unless ordered_by_date.has_key? start_date
+        ordered_by_date[start_date] = []
+      end
+      ordered_by_date[start_date] << event
+    end
+    ordered_by_date
+  end
 end
