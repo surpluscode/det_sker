6c1504094a44487fdfc2929b497dec6425c5121b
Ronan McHugh <mchugh.r@gmail.com>
2014-04-26 20:27:17 +0200

    #6 able to remove filters. display of active filters.

--- app/assets/javascripts/compatibility.js
+++ app/assets/javascripts/compatibility.js
@@ -34,3 +34,14 @@ if (!Array.prototype.indexOf) {
         return -1;
     };
 }
+
+Array.prototype.remove = function() {
+    var what, a = arguments, L = a.length, ax;
+    while (L && this.length) {
+        what = a[--L];
+        while ((ax = this.indexOf(what)) !== -1) {
+            this.splice(ax, 1);
+        }
+    }
+    return this;
+};
--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,32 +1,93 @@
+var activeFilters = [];
 
-    $(document).ready(function() {
-        $('div.event-container').click(toggleContent);
-        $('a.filter-link').click(filterCategory);
-    });
+$(document).ready(function() {
+    $('div.event-container').click(toggleContent);
+    $('a.filter-link').click(filterCategory);
+    //we need to handle the click on the ul level,
+    // because the li elements aren't generated  yet
+    $('ul.active-filters_js').click(removeFilter);
+
+});
 
 
-    /**
-     * When a user clicks on a filter-link
-     * hide all events that do not have that category
-     * @returns {boolean}
-     */
-    function filterCategory(){
-        var filter = $(this).attr('data-toggle');
-        console.log(filter);
-        $("div.event-container[data-category!=" + filter +"]").hide();
-        return false;
+
+/**
+ * When a user clicks on a filter-link
+ * hide all events that do not have that category
+ * and show filter as active in DOM
+ * @returns {boolean}
+ */
+function filterCategory(){
+    var filter = $(this).attr('data-toggle');
+    if (activeFilters.indexOf(filter) == -1) {
+        activeFilters.push(filter);
+        updateActiveFilters();
+        evaluateShown();
+    }
+    return false;
+}
+
+
+function removeFilter(event){
+    var filter = $(event.target).attr('data-toggle');
+    activeFilters.remove(filter);
+    updateActiveFilters();
+    evaluateShown();
+    return false;
+}
+
+/**
+ * if there is at least one active filter
+ * hide all events that do not have a category matching our filters
+ * else show all events
+ */
+function evaluateShown(){
+    var events = $('div.event-container');
+    if (activeFilters.length == 0) {
+        events.show();
+        return;
     }
 
-    /*
-     *   If this container's details div is exposed, hide it
-     *   otherwise, hide any exposed details div and show the
-     *   current container's details div.
-     */
-    function toggleContent() {
-        if  ($(this).children('.event-details').hasClass('revealed')) {
-            $(this).children('.event-details').removeClass('revealed').slideToggle();
-        } else {
-            $('.revealed').removeClass('revealed').slideToggle();
-            $(this).children('.event-details').addClass('revealed').slideToggle();
+    events.each(function(i){
+        var cat = $(this).attr('data-category');
+        if ($(this).is(':hidden')) {
+            if (activeFilters.indexOf(cat) >= 0) {
+                $(this).show();
+            }
+        } else if ($(this).is(':visible')) {
+            console.log( cat + "visible");
+            if (activeFilters.indexOf(cat) == -1) {
+                $(this).hide();
+            }
         }
+    });
+}
+
+/**
+* Given a change in filter state
+* Ensure that currently active filters
+ * are represented in the DOM
+*/
+function updateActiveFilters(){
+    var filterHTML = '';
+    for (var i = 0; i < activeFilters.length; i++) {
+        var filterName = activeFilters[i];
+        var removeLink = ' <a href="" class="remove-filter" data-toggle="' + filterName + '"> [X] </a>';
+        filterHTML += '<li>' + filterName + removeLink + '</li>';
     }
+    $('ul.active-filters_js').html(filterHTML);
+}
+
+/**
+ *   If this container's details div is exposed, hide it
+ *   otherwise, hide any exposed details div and show the
+ *   current container's details div.
+ */
+function toggleContent() {
+    if  ($(this).children('.event-details').hasClass('revealed')) {
+        $(this).children('.event-details').removeClass('revealed').slideToggle();
+    } else {
+        $('.revealed').removeClass('revealed').slideToggle();
+        $(this).children('.event-details').addClass('revealed').slideToggle();
+    }
+}
