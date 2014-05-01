0609973a896773359d4e2139b52c5d7b4b61d88e
Ronan McHugh <mchugh.r@gmail.com>
2014-05-01 22:17:50 +0200

    #27 - refactor - tests should not be dependent on html structure. remove need for fixtures.

--- features/events.feature
+++ features/events.feature
@@ -4,7 +4,7 @@ Feature: The index page
   Should be able to see events
 
 Scenario: The application has an index page
+  Given an event with title "Sample event" and description "Sample description" has been created
   When a user visits the home page
-  Then event title should be visible
-  But event description should be hidden
-  But event details should be hidden
+  Then the text "Sample event" should be visible
+  But the text "Sample description" should be hidden
--- features/step_definitions/event_steps.rb
+++ features/step_definitions/event_steps.rb
@@ -1,19 +1,25 @@
 When(/^a user visits the home page$/) do
   visit root_path
 end
+Given (/^an event with title "(.*)" and description "(.*)" has been created$/) do |title, description|
+  @event_details = { title: title,
+                    creator: 'FestAbe99',
+                    description: description,
+                    location: 'ungdomshuset',
+                    start_time: DateTime.new,
+                    end_time: DateTime.new,
+                    category: 'party' }
+  Event.create(@event_details)
+end
 
 Then(/^they should see events$/) do
   page.has_selector?('.event-container')
 end
 
-And(/^(.*) should be visible$/) do |classname|
-  page.has_selector?(convert_to_selector(classname), visible: true)
+And(/^the text "(.*)" should be visible$/) do |text|
+  page.has_text?(text, visible: true)
 end
 
-But(/^(.*) should be hidden$/) do |classname|
-  page.has_selector?(convert_to_selector(classname), visible: false)
-end
-
-def convert_to_selector(text)
-  '.' + text.gsub(' ', '-')
+But(/^the text "(.*)" should be hidden$/) do |text|
+  page.has_text?(text, visible: false)
 end
