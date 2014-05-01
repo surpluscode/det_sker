3a9771ddb66df7c6d23c690de4ed7f8fbf2a24c9
Ronan McHugh <mchugh.r@gmail.com>
2014-05-01 14:27:47 +0200

    #27 - little bit more detail.

--- features/step_definitions/event_steps.rb
+++ features/step_definitions/event_steps.rb
@@ -1,9 +1,8 @@
-#require 'webrat'
-
 When(/^a user visits the home page$/) do
   visit root_path
 end
 
 Then(/^they should see events$/) do
   page.has_selector?('.event-container')
+  page.has_selector?('.event-details')
 end
