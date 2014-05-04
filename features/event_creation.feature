3e7f9a1c63310ca48abc786331da44da82eec667
Ronan McHugh <mchugh.r@gmail.com>
2014-05-04 22:55:14 +0200

    #27 - cucumber test for event creation.

--- features/event_creation.feature
+++ features/event_creation.feature
@@ -3,5 +3,7 @@ Feature: Event creation
 
   Scenario: The application allows users to create events
     When a user visits the home page
-    And they click on the New link
-   # Then they should see the New event form
+    And they click on the New Event link
+    Then the form "new_event" should be visible
+    When they fill in the form
+    Then the text "Sample title" should be visible
--- features/step_definitions/event_steps.rb
+++ features/step_definitions/event_steps.rb
@@ -23,3 +23,36 @@ end
 But(/^the text "(.*)" should be hidden$/) do |text|
   page.has_text?(text, visible: false)
 end
+
+And(/^they click on the (.*) link$/) do |name|
+  click_link(name)
+end
+
+Then(/^the form "([^"]*)" should be visible$/) do |id|
+  selector = '#' + id
+  page.has_selector?(selector, visible: true)
+end
+
+When(/^they fill in the form$/) do
+  within '#new_event' do
+    fill_in 'event_title', with: 'Sample title'
+    fill_in 'event_creator', with: 'Sample creator'
+    fill_in 'event_description', with: 'Some gibberish'
+    select 'folkets', from: 'event_location'
+    select 'party', from: 'event_category'
+    now = DateTime.now
+    select now.year, from: 'event_start_time_1i'
+    select now.strftime('%B'), from: 'event_start_time_2i'
+    select now.day, from: 'event_start_time_3i'
+    select now.hour, from: 'event_start_time_4i'
+    select now.minute, from: 'event_start_time_5i'
+
+    select now.year, from: 'event_end_time_1i'
+    select now.strftime('%B'), from: 'event_end_time_2i'
+    select now.day + 1, from: 'event_end_time_3i'
+    select now.hour, from: 'event_end_time_4i'
+    select now.minute, from: 'event_end_time_5i'
+
+    click_button 'Create'
+  end
+end
