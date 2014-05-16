78136075e7091b8f202071cdc5c4dd2e6619b9e2
Ronan McHugh <mchugh.r@gmail.com>
2014-05-16 11:40:41 +0200

    #42 - add user association to event model.

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,7 @@ class Event < ActiveRecord::Base
   validates :title, :short_description, :location, :start_time, :end_time, :category, :creator, presence: true
   validates_inclusion_of :category, in: DetSker::Application.config.possible_categories
   validates_inclusion_of :location, in: DetSker::Application.config.possible_locations
+  belongs_to :user
 
   # This method returns the index view
   # used by EventController to list all events
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140516075331) do
+ActiveRecord::Schema.define(version: 20140516092542) do
 
   create_table "events", force: true do |t|
     t.string   "title"
@@ -26,7 +26,10 @@ ActiveRecord::Schema.define(version: 20140516075331) do
     t.string   "price"
     t.boolean  "cancelled"
     t.text     "long_description"
+    t.integer  "user_id"
   end
+
+  add_index "events", ["user_id"], name: "index_events_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
