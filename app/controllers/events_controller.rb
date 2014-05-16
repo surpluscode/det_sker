81d53e22e41a8b414c0ab5704ae3781ebc2090e9
Ronan McHugh <mchugh.r@gmail.com>
2014-05-16 10:45:56 +0200

    #42 - add auth hooks to controller.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
 
   def index
     @events_by_date = Event.index
