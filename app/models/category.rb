59fff02f8316ff4e489f6ec9c5ab617b9d11d6a3
Ronan McHugh <mchugh.r@gmail.com>
2014-06-03 23:10:18 +0200

    #34 - Use VERY DIRTY raw sql to query categories_events relation directly.

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,8 +3,14 @@ class Category < ActiveRecord::Base
   has_and_belongs_to_many :events
 
   # Return a grouped set of categories containing current events
+  # we need to use raw SQL here because we don't have a CategoriesEvents model
+  # we can refer to.
+  # This is dangerous because the SQL might not be compatible across dbs
   def self.current_categories
-     self.select('count(*) as num').includes(:events)
-     .where('events.end_time > ?', DateTime.now).references(:events).group(:key)
+     ActiveRecord::Base.connection.execute(
+         "SELECT c.key, COUNT(*) AS num from categories_events AS ce, events AS e, categories AS c
+          WHERE ce.event_id = e.id AND ce.category_id = c.id
+          AND e.end_time > DATETIME('now')"
+     )
   end
 end
