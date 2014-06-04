1d1f04ccd913b81cc34eab4f155c56845cc293b4
Ronan McHugh <mchugh.r@gmail.com>
2014-06-04 20:00:27 +0200

    #34 - add default categories to db seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.create([{ key: :party }, { key: :food }, { key: :exercise },
+                 { key: :workshop }, { key: :demo }, { key: :talk },
+                { key: :meeting }, { key: :other }])
