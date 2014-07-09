2d1981cffdaf0b8be6d4254048c1a1e7c2e83dbb
Ronan McHugh <mchugh.r@gmail.com>
2014-07-09 23:05:56 +0200

    test for event controller to ensure it returns comments.

---
+++ spec/controllers/event_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe EventsController do
+
+  describe 'GET#show' do
+
+    it 'should return the event itself' do
+      event = FactoryGirl.create(:event)
+      get :show, id: event
+      assigns(:event).should eql event
+    end
+
+    it 'should return all the events comments' do
+      event = FactoryGirl.create(:event)
+      c = FactoryGirl.create(:comment)
+      event.comments << c
+      get :show, id: event
+
+      assigns(:event).comments.first.should eql c
+    end
+  end
+
+end
