98a19a4ddc863076adb40a672f19dc733044cd3b
Ronan McHugh <mchugh.r@gmail.com>
2014-07-23 21:23:28 +0200

    update spec and model to ensure event and user are present.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :event
   validates_associated :user, :event
+  validates :user, :event, presence: true
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,11 +2,17 @@ require 'spec_helper'
 
 describe 'Comment' do
 
-  it 'should create a comment' do
+  it 'should create a comment when given valid params' do
     u = FactoryGirl.create(:user)
     e = FactoryGirl.create(:event)
     comment = Comment.new(content: 'Sample comment', hidden: false, user: u, event: e)
-    comment.should be_a Comment
+    comment.save.should be_true
     comment.content.should eql 'Sample comment'
+  end
+
+  it 'should not create a comment without an associated event' do
+    u = FactoryGirl.create(:user)
+    comment = Comment.new(content: 'Sample comment', hidden: false, user: u)
+    comment.save.should_not be_true
   end
 end
