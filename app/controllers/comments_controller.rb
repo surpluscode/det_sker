64aa6a7d7b09e77677f402ef6875a79b4ddd7930
Ronan McHugh <mchugh.r@gmail.com>
2014-07-23 23:32:40 +0200

    enable adding comments to event pages.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,7 @@ class CommentsController < ApplicationController
     comment_params = user_params
     # add the user id if we've got one
     comment_params.merge!(user_id: current_user.id) if user_signed_in?
+    logger.debug comment_params.inspect
 
     @comment = Comment.new(comment_params)
     respond_to do |format|
@@ -11,7 +12,7 @@ class CommentsController < ApplicationController
         format.html { redirect_to root_path, notice: 'Comment was created successfully.'}
         format.json { render json: @comment, status: :created}
       else
-        format.html { redirect_to root_path, notice: 'Comment was not created.' }
+        format.html { redirect_to root_path, notice: "Comment was not created: #{@comment.errors.inspect}" }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
@@ -19,7 +20,16 @@ class CommentsController < ApplicationController
 
   private
 
+  # Little bit hacky - since we need params from both the nested
+  # comments hash and the upper level we need to go through an
+  # extra layer of processing, e.g. given this:
+  # {"comment"=>{"content"=>"first comment!"}, "event_id"=>"989132030"}
+  # we want this:
+  # {"event_id"=>"989132030", "content"=>"first comment!"}
   def user_params
-    params.require(:comment).permit(:content, :event_id)
+    params.permit(:event_id, comment: [:content]).tap do |list|
+      list[:content] = list[:comment][:content]
+      list.delete(:comment)
+    end
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -10,7 +10,7 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :event
   validates_associated :user, :event
-  validates :event, :user, presence: true
+  validates :event, presence: true
   validates_with ValidUser
 
 end
