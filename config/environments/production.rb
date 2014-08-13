905f7324d29689aace5e0e7d930090412657c876
Ronan McHugh <mchugh.r@gmail.com>
2014-08-13 21:42:43 +0200

    configure SendGrid mailer for Heroku.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,16 @@ DetSker::Application.configure do
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
+  config.action_mailer.default_url_options = { host: 'thawing-dawn-8343.herokuapp.com' }
+  config.action_mailer.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com',
+      :enable_starttls_auto => true
+  }
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
