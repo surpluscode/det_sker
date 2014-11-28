889e41510c89f02989bbab6436b26667bee356e4
Ronan McHugh <mchugh.r@gmail.com>
2014-11-28 10:18:49 +0100

    #107 - Refactor anonymous user flow to make it more Railsy.  Remove outdated test.

--- app/controllers/anonymous_user_controller.rb
+++ app/controllers/anonymous_user_controller.rb
@@ -1,18 +1,22 @@
 class AnonymousUserController < ApplicationController
   def new
-    @user = User.new
+    @user = User.new(password: 'fake_password')
   end
 
   # given a username, create a one time user and allow it to create events
   def create
     allow_params_authentication!
-    u = AnonymousUser.create_anonymous_user(whitelist_params)
-    sign_in u
-    if warden.authenticate!(scope: :user)
-      warden.set_user u
+    anon_params = AnonymousUser.attributes(whitelist_params)
+    @user = User.new(anon_params)
+    if @user.save
+      # since we're circumventing the usual Devise flow here
+      # we need to sign in like this
+      sign_in @user
+      warden.authenticate!(scope: :user)
+      warden.set_user @user
       redirect_to new_event_path
     else
-      redirect_to :back
+      render action: :new, status: :unprocessable_entity
     end
   end
 
--- app/models/anonymous_user.rb
+++ app/models/anonymous_user.rb
@@ -1,11 +1,9 @@
-class AnonymousUser < User
-  # Given a hash with a username
-  # create an anonymous user with
-  # default values
-  def self.create_anonymous_user(attrs)
-    random_str = Time.now.to_i + rand(99)
-    attrs.merge!(email: "#{random_str}@anon.com", confirmed_at: DateTime.now,
-      encrypted_password: "#{random_str}", is_admin: false, is_anonymous: true )
-    User.create(attrs) unless attrs[:username].nil?
+class AnonymousUser
+  # Create attributes for anonymous user
+  # based on a given username
+  def self.attributes(attrs)
+    random_str = Time.now.to_i.to_s + (0...16).map { (65 + rand(26)).chr }.join
+    attrs.merge(email: "#{random_str}@anon.com", confirmed_at: DateTime.now,
+                 password: random_str, is_admin: false, is_anonymous: true )
   end
 end
