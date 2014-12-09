0f2b285a5c58b23a4623e20c7938604cd5d79fdd
Ronan McHugh <mchugh.r@gmail.com>
2014-12-09 22:31:29 +0100

    Ensure locale is persisted between requests.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_locale
 
+  def default_url_options(options={})
+    { locale: I18n.locale }
+  end
+
   protected
 
   def set_locale
