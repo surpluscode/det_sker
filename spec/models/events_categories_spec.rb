2770b0580f318e5ff7e279373b992919416de897
Ronan McHugh <mchugh.r@gmail.com>
2015-03-08 22:59:26 +0100

    bug in test.

--- spec/models/events_categories_spec.rb
+++ spec/models/events_categories_spec.rb
@@ -4,7 +4,7 @@ describe 'Events to Categories relationship' do
 
   it 'should maintain associations between events and categories' do
     event = FactoryGirl.create(:event)
-    category = FactoryGirl.create(:demo_cat, key: Time.now.to_i)
+    category = FactoryGirl.create(:demo_cat, english: Time.now.to_i)
     event.categories << category
     event.categories.count.should eql 1
     category.events.count.should eql 1
