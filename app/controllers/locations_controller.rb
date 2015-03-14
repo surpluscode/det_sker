e77b56c156201f89d310d8f1e56d110848f6ae8e
Ronan McHugh <mchugh.r@gmail.com>
2015-03-14 22:49:52 +0100

    Use generic template for rendering modal success messages.

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -23,8 +23,8 @@ class LocationsController < ApplicationController
     @location = Location.new(location_whitelist)
     respond_to do |format|
       if @location.save
+        format.html { render 'layouts/created', layout: nil }
         format.json { render json: @location, status: :created}
-        format.html { redirect_to @location, notice: 'Location saved successfully' }
       else
         format.html { render action: :new, status: :unprocessable_entity, layout: nil }
         format.json { render json: @location.errors, status: :unprocessable_entity }
