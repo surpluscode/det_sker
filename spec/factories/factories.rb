5b429fde37fc8af92b866cfa40c6652ecf77cabe
Ronan McHugh <mchugh.r@gmail.com>
2015-03-15 21:36:34 +0100

    fix factory for category validation rule.

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -31,6 +31,7 @@ FactoryGirl.define do
     end_time DateTime.now + 5.hours
     location
     user_id 1
+    categories {[FactoryGirl.create(:random_category)]}
 
     factory :event_yesterday do
       title 'This happened yesterday'
