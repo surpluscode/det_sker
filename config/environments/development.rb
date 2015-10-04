b33bf2ce2e17a0e31c672db755b8cf359d86e00b
Ronan McHugh <mchugh.r@gmail.com>
2015-10-04 21:07:45 +0200

    enable caching in dev mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,4 +27,5 @@ DetSker::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.action_controller.perform_caching = true
 end
