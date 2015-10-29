0dca1e1c37214c19e1a5b1f9465729cc1e1f5bc5
Ronan McHugh <mchugh.r@gmail.com>
2015-10-29 13:08:23 +0100

    #218 add description as part of creation flow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :username
+    devise_parameter_sanitizer.for(:sign_up) << :username << :description
   end
 
   def user_can_edit?(object)
