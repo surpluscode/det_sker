a423e9be409ec8959563008bb04fa5212b08565f
Ronan McHugh <mchugh.r@gmail.com>
2015-12-02 22:37:18 +0100

    remove trackable

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :validatable
   alias_method :user_id, :id
 
   validates :username, presence: true, uniqueness: true
