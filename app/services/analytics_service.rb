2daa43d11067c0a74051111d2ad87a8820211e46
Ronan McHugh <mchugh.r@gmail.com>
2016-05-29 12:55:29 +0200

    use both percentage and absolute counts for vist groups

--- app/services/analytics_service.rb
+++ app/services/analytics_service.rb
@@ -1,5 +1,11 @@
 class AnalyticsService
+  # Return a sorted Hash of fields grouped by field
+  # with counts represented as both absolute and as percentage
+  # e.g. { 'calendar#index' => [3, 100.0] }
   def self.group_by(field)
-    Ahoy::Event.group(field).order('count_id desc').count(:id)
+    grouped = Ahoy::Event.group(field).order('count_id desc').count(:id)
+    total_count = grouped.values.inject(&:+)
+    factor = 100.0 / total_count.to_f
+    grouped.transform_values {|val| [val, (val.to_f) * factor] }
   end
 end
--- spec/services/analytics_spec.rb
+++ spec/services/analytics_spec.rb
@@ -9,8 +9,23 @@ describe AnalyticsService do
     end
     subject { described_class.group_by(:name) }
     it { should be_a Hash }
+    it 'should group by name' do
+      expect(subject.values.size).to eql 2
+    end
     it 'should be ordered by count' do
       expect(subject.first.first).to eql 'calendar#index'
+    end
+    it 'returns values as an array' do
+      expect(subject.values.first).to be_an Array
+    end
+    it 'returns the absolute count as the first value' do
+      expect(subject.values.first.first).to eql 2
+    end
+    it 'returns the percentage as the second value' do
+      expect(subject.values.first.second.round(0)).to eql 67
+    end
+    it 'should render percentage values' do
+      expect(subject.values.collect(&:last).inject(&:+)).to eql 100.0
     end
   end
 end
